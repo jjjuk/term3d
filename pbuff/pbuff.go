ee60a1f1534dda21071a467feceefe8bbda4b472
jjjyk <gmodhl67@gmail.com>
2023-09-14 20:20:35 +0300

    remove useless mutex

--- pbuff/pbuff.go
+++ pbuff/pbuff.go
@@ -2,33 +2,29 @@ package pbuff
 
 import (
 	"errors"
-	"sync"
 )
 
 type PlaneBuffer struct {
 	Buffer []byte
-	mu     sync.Mutex
 	xSize  uint
 	ySize  uint
 }
 
 func New(x, y uint) *PlaneBuffer {
-	return &PlaneBuffer{Buffer: make([]byte, x*y, x*y), xSize: x, ySize: y}
+	return &PlaneBuffer{Buffer: make([]byte, x*y), xSize: x, ySize: y}
 }
 
 func (c *PlaneBuffer) getLinPos(x, y uint) (uint, error) {
 	if x > c.xSize || y > c.ySize {
-		return 0, errors.New("Position is out of range")
+		return 0, errors.New("position is out of range")
 	}
 	return y*c.xSize + x, nil
 }
 
 func (c *PlaneBuffer) Set(x, y uint, char byte) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	pos, err := c.getLinPos(x, y)
 	if err != nil {
-		return
+		return // maybe panic
 	}
 	c.Buffer[pos] = char
 }
